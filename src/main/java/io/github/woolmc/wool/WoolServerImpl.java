376e01effae795f369a6b2b5ac48ffd2f26b9c6b
Devan-Kerman <devan@cleverpath.com>
2019-10-13 12:49:49 -0500

    missed one

--- src/main/java/io/github/woolmc/wool/WoolServerImpl.java
+++ src/main/java/io/github/woolmc/wool/WoolServerImpl.java
@@ -118,7 +118,7 @@ public class WoolServerImpl implements Server {
 
 	@Override
 	public Set<String> getListeningPluginChannels() {
-		
+		throw new UnsupportedOperationException("unsupported");
 	}
 
 	@Override
